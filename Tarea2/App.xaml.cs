5100a1a5da2993c32420f097c68ae657d625721e
ilopez78 <99553835+ilopez78@users.noreply.github.com>
2022-07-15 13:53:36 -0600

    Agregar archivos de proyecto.

---
+++ Tarea2/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xamarin.Forms;
+using Tarea2.DataBase;
+using System.IO;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea2
+{
+    public partial class App : Application
+    {
+        static SQLiteHelper db;
+        public App()
+        {
+            InitializeComponent();
+
+            // MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
+        }
+        public static SQLiteHelper SQLiteDB
+        {
+            get
+            {
+                if (db == null)
+                {
+                    db = new SQLiteHelper(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Persona.db3"));
+                }
+                return db;
+            }
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
